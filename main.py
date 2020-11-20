0e937c5b5298da88c053ad49a4247f024d701c0e
yonixiong <yoni.xiong@vanderbilt.edu>
2020-11-19 18:14:49 -0600

    made file for main program

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import sys
+sys.path.append(os.path.relpath("./libraries"))
+
+# uncomment once "import smbus" works 
+# from decision import *
+# from Line_Tracking import *
+# from Motor import *
+# from PCA9685 import *
+# from servo import *
+# from slow import *
+# from ultrasonic_sensor import *
+from network.client.src.NetworkPackage import *
+
+# Autonomous Line Tracking Car
