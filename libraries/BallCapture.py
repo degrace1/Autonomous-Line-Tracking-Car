22cc2e9a22c50f8d170f5833fe8d131846521f9e
duyubo <2669561343@qq.com>
2020-11-29 20:45:05 -1000

    add save image function

--- libraries/BallCapture.py
+++ libraries/BallCapture.py
@@ -7,12 +7,15 @@ import time
 import math 
 from imutils.video import VideoStream
 from datetime import datetime
-
+import os
 '''given the fame (image of each capture)'''
 class BallCapture:
     
-    def __init__(self,yellowLower = (26,43,46), yellowUpper = (34,255,255), minR = 0, maxR = 80):
-        self.vs = VideoStream(src=0).start()
+    def __init__(self,yellowLower = (26,43,46), yellowUpper = (34,255,255), minR = 0, maxR = 80, vs = None):
+        if vs == None:
+            self.vs = VideoStream(src=0).start()
+        else:
+            self.vs = vs
         self.yellowLower = yellowLower
         self.yellowUpper = yellowUpper
         self.minR = minR
@@ -59,8 +62,8 @@ class BallCapture:
             cv2.imshow("Frame", frame)
             cv2.imshow("Mask", mask)
             key = cv2.waitKey(1) & 0xFF
-            cv2.imwrite('p'+datetime.now().strftime("%d/%m/%Y %H:%M:%S")+'.jpg',frame)
-        
+            cv2.imwrite("frame//"+datetime.now().strftime("%d-%m-%Y-%H-%M-%S-%f")+'.jpg',frame)
+            cv2.imwrite("mask//"+datetime.now().strftime("%d-%m-%Y-%H-%M-%S-%f")+'.jpg',mask)
         return x,y,radius
     
     # compute the distance of a car given radius of the ball
@@ -81,6 +84,8 @@ class BallCapture:
             return result[0],result[1],result[2],dis
         else:
             return 0,0,0,-1
+        
+    #close the camera and all windows
     def endAll(self):
         self.vs.stop()
         cv2.destroyAllWindows() 
