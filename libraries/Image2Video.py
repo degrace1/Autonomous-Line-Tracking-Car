9f19bf00b45f530cccbf46560d281e3afc00357f
duyubo <2669561343@qq.com>
2020-11-30 09:43:20 -1000

    add the assembling inages into cideo module

--- libraries/Image2Video.py
+++ libraries/Image2Video.py
@@ -1,11 +1,19 @@
 import cv2
 import numpy as np
 import os
+from datetime import datetime,date,timedelta
 
 img_array = []
-for filename in os.listdir('.\frame'):
+names = os.listdir('./frame')
+times =sorted([datetime.strptime(dt.split('.')[0], "%d-%m-%Y-%H-%M-%S-%f") for dt in names])
+ImageNames = []
+for d in times:
+    ImageNames.append(d.strftime("%d-%m-%Y-%H-%M-%S-%f")+'.jpg')
+#print(ImageNames)
+
+for filename in ImageNames:
     print(filename)
-    img = cv2.imread(filename)
+    img = cv2.imread('./frame//'+filename)
     height, width, layers = img.shape
     size = (width,height)
     img_array.append(img)
