083c2493dcd7c8e23f9a08a61b0c32059517b3d4
duyubo <2669561343@qq.com>
2020-11-30 09:40:34 -1000

    add the assembling inages into cideo module

---
+++ libraries/Image2Video.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+import os
+
+img_array = []
+for filename in os.listdir('.\frame'):
+    print(filename)
+    img = cv2.imread(filename)
+    height, width, layers = img.shape
+    size = (width,height)
+    img_array.append(img)
+
+
+out = cv2.VideoWriter('sample.avi',cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+ 
+for i in range(len(img_array)):
+    out.write(img_array[i])
+out.release()
