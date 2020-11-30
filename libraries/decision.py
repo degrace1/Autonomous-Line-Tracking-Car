b40ff150f753a843d1cb94cdbc327a15568ce853
duyubo <2669561343@qq.com>
2020-11-30 06:14:32 -1000

    fix somebugs and add some output

--- libraries/decision.py
+++ libraries/decision.py
@@ -2,14 +2,18 @@ import time
 from Motor import *
 from Line_Tracking import *
 from slow import Slow_Tracking
-from NetworkPackage import *
+from network.client.src.NetworkPackage import *
+from BallCapture import *
+import cv2
 
 class Decision:
     def __init__(self, id):
-        self.car = CarState(id) # Initialize car state object
+        self.car = CarState(id,0,0,0,0,0,0) # Initialize car state object
         self.line = Line_Tracking() # Car line follower algorithm
         self.time = time.time()
-    
+        self.vs =VideoStream(src=0).start()
+        self.BallTrack = BallCapture(vs = self.vs) #init the object detection class
+        
     def run(self):
         try:
             if self.car.getID == 0: # Car only traces line
@@ -24,6 +28,7 @@ class Decision:
     # Car has ID 0 and has the highest priority to the track. This car will follow
     # the line with no interruptions. 
     def highPriority(self):
+        print("Priority of thr car is high, just keep running!")
         self.line.run()
     
     # Car has ID 1 and has the second highest priority to the track. This car will
@@ -33,35 +38,53 @@ class Decision:
     def mediumPriority(self):
         slow = Slow_Tracking()
         while True:
-            if self.car.getUltrasonic < 30: # Ultrasonic sensor detects an object that is close
-                while self.car.getUltrasonic < 50:
+            if self.car.getUltrasonic() < 30: # Ultrasonic sensor detects an object that is close
+                print("ultrasonic find an object near!","distance = ", self.car.getUltrasonic())
+                while self.car.getUltrasonic() < 50:
+                    print("the object is still close (from ultrasonic)!","distance = ", self.car.getUltrasonic())
+                    frame = self.vs.read()
+                    cv2.imwrite("frame//"+datetime.now().strftime("%d-%m-%Y-%H-%M-%S-%f")+'.jpg',frame)
                     slow.run()
-            elif True: # Camera detects an object that is close
+            elif self.BallTrack.captureOne[3] < 30 and self.BallTrack.captureOne[3] > 0: # Camera detects an object that is close
+                print("camera find an object near!")
                 start = time.time()
                 update = time.time()
-                while True or update - start < 5: # While camera sees something to the side or 5 s has not elapsed
+                while (self.BallTrack.captureOne[3] < 30 and self.BallTrack.captureOne[3] > 0) or update - start < 5: # While camera sees something to the side or 5 s has not elapsed
+                    print("the object is still close!")
                     PWM.setMotorModel(0, 0, 0, 0)
                     update = time.time()
                     # get distance from camera
             else: # Nothing in the way, run normally
                 self.line.run()
-
+                frame = self.vs.read()
+                cv2.imwrite("frame//"+datetime.now().strftime("%d-%m-%Y-%H-%M-%S-%f")+'.jpg',frame)
+                print("keep running!")
     # Car has ID 2 and has the third highest priority to the track. This car will follow
     # the line. If the ultrasonic sensor detects an object within 30 cm, the car will stop
     # until it's at least 50 cm away. If the camera de
     def lowPriority(self):
         while True:
             if self.car.getUltrasonic() < 30: # Ultrasnoic sensor detects that an object is close
-                while self.car.getUltrasonic < 50:
+                print("ultrasonic find an object near!","distance = ", self.car.getUltrasonic())
+                while self.car.getUltrasonic() < 50:
+                    print("the object is still close (from ultrasonic)!","distance = ", self.car.getUltrasonic())
+                    frame = self.vs.read()
+                    cv2.imwrite("frame//"+datetime.now().strftime("%d-%m-%Y-%H-%M-%S-%f")+'.jpg',frame)
                     PWM.setMotorModel(0, 0, 0, 0) # Stop
-            elif True: # Camera detects that an object is close
-                while True: #FIXME - distance to car must be less than 50 cm-ish
+            elif self.BallTrack.captureOne[3] < 30 and self.BallTrack.captureOne[3] > 0: # Camera detects that an object is close
+                print("camera find an object near!")
+                while self.BallTrack.captureOne[3] < 50 and self.BallTrack.captureOne[3] > 0: #FIXME - distance to car must be less than 50 cm-ish
+                    print("the object is still close!")
+                    PWM.setMotorModel(0, 0, 0, 0)
             else: # Nothing in the way, run normally
-                self.line.run()       
+                self.line.run()
+                print("nothing in the way, keep running!")
+                frame = self.vs.read()
+                cv2.imwrite("frame//"+datetime.now().strftime("%d-%m-%Y-%H-%M-%S-%f")+'.jpg',frame)
 
 
 ## TEST CODE
-id = 0 # Car ID changes its priority
+id = 1 # Car ID changes its priority
 decision = Decision(id)
 if __name__ == '__main__':
     print('Decision algorithm is starting...')
