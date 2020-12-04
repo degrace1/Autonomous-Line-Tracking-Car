98c67867ea63ac892da766347875f5b783079f60
Alex Feeley <alexandra.feeley.lamb@vanderbilt.edu>
2020-12-03 19:53:03 -0600

    Update decision.py
    Changed terminal output to be more informative

--- libraries/decision.py
+++ libraries/decision.py
@@ -26,15 +26,15 @@ class Decision:
     def run(self):
         try:
             if self.car.getID() == 0: # Car only traces line
-                print('the car is high priority')
+                print('This car has high priority.')
                 self.highPriority()
                 
             elif self.car.getID() == 1: # Car stops for one car or 5s
-                print('the car is medium priority')
+                print('This car has medium priority.')
                 self.mediumPriority()
                 
             else: # Car stops until no cars are in view
-                print('the car is low priority')
+                print('This car has low priority.')
                 self.lowPriority()
                 
         except KeyboardInterrupt: 
@@ -43,10 +43,10 @@ class Decision:
     # Car has ID 0 and has the highest priority to the track. This car will follow
     # the line with no interruptions. 
     def highPriority(self):
-        print("Priority of the car is high, just keep running!")
+        print("This car has priority on the track over every other car. Keep running!")
         while True:
             frame = self.vs.read()
-            cv2.putText(frame, text='Priority of the car is high, just keep running!', org=self.label,
+            cv2.putText(frame, text='High Priority Car', org=self.label,
             fontFace= cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0,0,255),
             thickness=2, lineType=cv2.LINE_AA)
             cv2.imwrite("frame//"+datetime.now().strftime("%d-%m-%Y-%H-%M-%S-%f")+'.jpg',frame)
@@ -59,32 +59,35 @@ class Decision:
     def mediumPriority(self):
         slow = Slow_Tracking()
         while True:
-            if self.car.getUltrasonic() < 30: # Ultrasonic sensor detects an object that is close
-                print("ultrasonic finds an object near!","distance = ", self.car.getUltrasonic())
-                while self.car.getUltrasonic() < 50:
-                    print("the object is still close (from ultrasonic)!","distance = ", self.car.getUltrasonic())
+            if self.car.getUltrasonic() < 20: # Ultrasonic sensor detects an object that is close
+                print("There is an object within 20 cm of the car, slow down!")
+                while self.car.getUltrasonic() < 25:
+                    print("There is an object ", self.car.getUltrasonic(), " cm away from the car.")
                     frame = self.vs.read()
-                    cv2.putText(frame, text="object is still close (from ultrasonic)!", org=self.label,
+                    cv2.putText(frame, text="There is an object close by.", org=self.label,
                     fontFace= cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0,0,255),
                     thickness=2, lineType=cv2.LINE_AA)
-                    cv2.putText(frame, text="distance = "+ str(self.car.getUltrasonic()), org=(self.label[0],self.label[1]+30),
+                    cv2.putText(frame, text="Distance: " + str(self.car.getUltrasonic()), org=(self.label[0],self.label[1]+30),
                     fontFace= cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0,0,255),
                     thickness=2, lineType=cv2.LINE_AA)
                     cv2.imwrite("frame//"+datetime.now().strftime("%d-%m-%Y-%H-%M-%S-%f")+'.jpg',frame)
                     slow.run()
+                    
             elif self.BallTrack.captureOne()[3] < 30 : # Camera detects an object that is close
+                slow = Slow_Tracking()
                 PWM.setMotorModel(0, 0, 0, 0)
-                print("camera finds an object near!")
+                print("There is an object within 30 cm of the car, slow down!")
                 start = time.time()
                 update = time.time()
                 while (self.BallTrack.captureOne()[3] < 30 ) or update - start < 5: # While camera sees something to the side or 5 s has not elapsed
-                    print("the object is still close!")
+                    print("There is an object ", self.BallTrack.captureOne()[3], " cm away from the car.")
                     PWM.setMotorModel(0, 0, 0, 0)
                     update = time.time()
-                    # get distance from camera
+                    slow.run()
+                    
             else: # Nothing in the way, run normally
                 frame = self.vs.read()
-                cv2.putText(frame, text="nothing detected, keep running", org=self.label,
+                cv2.putText(frame, text="No objects are detected.", org=self.label,
                     fontFace= cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0,0,255),
                     thickness=2, lineType=cv2.LINE_AA)
                 cv2.imwrite("frame//"+datetime.now().strftime("%d-%m-%Y-%H-%M-%S-%f")+'.jpg',frame)
@@ -105,31 +108,27 @@ class Decision:
                 while self.car.getUltrasonic() < 30:
                     self.car.setUltrasonic() 
                     PWM.setMotorModel(0, 0, 0, 0) # Stop
-                    print("the object is still close (from ultrasonic)!","distance = ", self.car.getUltrasonic())
+                    print("There is an object ", self.car.getUltrasonic(), " cm away.")
                     frame = self.vs.read()
-                    cv2.putText(frame, text="object is still close (from ultrasonic)!", org=self.label,
+                    cv2.putText(frame, text="There is an object close by", org=self.label,
                     fontFace= cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0,0,255),
                     thickness=2, lineType=cv2.LINE_AA)
-                    cv2.putText(frame, text="distance = "+ str(self.car.getUltrasonic()), org=(self.label[0],self.label[1]+30),
+                    cv2.putText(frame, text="Distance: "+ str(self.car.getUltrasonic()), org=(self.label[0],self.label[1]+30),
                     fontFace= cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0,0,255),
                     thickness=2, lineType=cv2.LINE_AA)
                     cv2.imwrite("frame//"+datetime.now().strftime("%d-%m-%Y-%H-%M-%S-%f")+'.jpg',frame)
                     
-
-            
-
-            #elif self.BallTrack.captureOne()[3] < 30: # Camera detects that an object is close
-                
-                #PWM.setMotorModel(0, 0, 0, 0)
-                #print("camera finds an object near!")
-                #while self.BallTrack.captureOne()[3] < 50 : #FIXME - distance to car must be less than 50 cm-ish
-                    #print("the object is still close!")
-                    #PWM.setMotorModel(0, 0, 0, 0)
+            elif self.BallTrack.captureOne()[3] < 30: # Camera detects that an object is close
+                PWM.setMotorModel(0, 0, 0, 0)
+                print("camera finds an object near!")
+                while self.BallTrack.captureOne()[3] < 50 : #FIXME - distance to car must be less than 50 cm-ish
+                    print("the object is still close!")
+                    PWM.setMotorModel(0, 0, 0, 0)
                     
             else: # Nothing in the way, run normally
-                print("nothing in the way, keep running! branch 3")
+                print("No objects are detected. ")
                 frame = self.vs.read()
-                cv2.putText(frame, text="nothing detected, keep running", org = self.label,
+                cv2.putText(frame, text="No objects are detected.", org = self.label,
                     fontFace= cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0,0,255),
                     thickness=2, lineType=cv2.LINE_AA)
                 cv2.imwrite("frame//"+datetime.now().strftime("%d-%m-%Y-%H-%M-%S-%f")+'.jpg',frame)
